0dd8c0c64db27ed1ed8a9692a0b271a7e9895ab5
isinsuarici <isinsuu98@hotmail.com>
2023-01-16 01:25:31 +0300

    mock service is written to retrieve data

--- lib/api/mock_bookStore_service.dart
+++ lib/api/mock_bookStore_service.dart
@@ -1,6 +1,50 @@
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
+import '../models/models.dart';
 
-//import '../models/models.dart';
+class MockFooderlichService {
+  Future<ExploreData> getExploreData() async {
+    final bookInformations = await _getBookInformations();
+    final feedInformations = await _getFeedInformations();
 
+    return ExploreData(bookInformations, feedInformations);
+  }
+
+  Future<List<BookInfo>> _getBookInformations() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    final dataString =
+    await _loadAsset('assets/data_samples/bookInfo.json');
+    final Map<String, dynamic> json = jsonDecode(dataString);
+
+    if (json['bookInfo'] != null) {
+      final bookInfo = <BookInfo>[];
+      json['bookInfo'].forEach((v) {
+        bookInfo.add(BookInfo.fromJson(v));
+      });
+      return bookInfo;
+    } else {
+      return [];
+    }
+  }
+
+  Future<List<Feed>> _getFeedInformations() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    final dataString =
+    await _loadAsset('assets/data_samples/feed.json');
+    final Map<String, dynamic> json = jsonDecode(dataString);
+
+    if (json['feed'] != null) {
+      final feed = <Feed>[];
+      json['feed'].forEach((v) {
+        feed.add(Feed.fromJson(v));
+      });
+      return feed;
+    } else {
+      return [];
+    }
+  }
+
+  Future<String> _loadAsset(String path) async {
+    return rootBundle.loadString(path);
+  }
+}
