23e594e54c903741f16b3806cb4334604f7c431f
isinsuarici <isinsuu98@hotmail.com>
2023-01-17 23:04:16 +0300

    Null exception for AllBooks fixed.

--- lib/components/all_books_listView.dart
+++ lib/components/all_books_listView.dart
@@ -47,7 +47,11 @@ class Books extends StatelessWidget {
                       bookAuthor: book.bookAuthor,
                   bookPageNumber: book.bookPageNumber,
                   bookPublishedYear: book.bookPublishedYear,
-                      price1: book.price1,
+                      price1: book.price1, price2: book.price2,
+                    store1: book.store1,
+                    store2: book.store2,
+                    stock1:book.stock1,
+                    stock2:book.stock2,
                       bookSummary: book.bookSummary,)));
             },
             child: Row(
